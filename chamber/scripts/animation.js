4cfbcb828ac7fa1a49986b4d7b2458aeb97f6354
Norlando_jr <norlandojr@hotmail.com>
2025-03-29 09:03:21 -0300

    comit let's start debbuging

--- chamber/scripts/animation.js
+++ chamber/scripts/animation.js
@@ -1,29 +1,8 @@
-const modal = document.getElementById("modal");
-const closeBtn = document.getElementsByClassName("close")[0];
-const openModalBtn = document.getElementById("openModalBtn");
-
-// Open Modal
-openModalBtn.onclick = function() {
-    modal.style.display = "block";
-    setTimeout(function() {
-        modal.classList.add("show");  // Add show class to trigger animation
-    }, 10);
-};
-
-// Close Modal
-closeBtn.onclick = function() {
-    modal.classList.remove("show");  // Remove the show class to hide with animation
-    setTimeout(function() {
-        modal.style.display = "none";  // Hide the modal after animation completes
-    }, 300);  // Adjust timing to match animation duration
-};
-
-// Close Modal when clicking outside
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.classList.remove("show"); // Remove the show class to trigger animation
-        setTimeout(function() {
-            modal.style.display = "none";  // Hide the modal after animation completes
-        }, 300);
-    }
-};
+documedocument.addEventListener("DOMContentLoaded", function () {
+    let card = document.querySelector(".card");
+    card.style.animation = "fadeInUp 0.5s ease-out forwards";
+});
+nt.addEventListener("DOMContentLoaded", function () {
+    let card = document.querySelector(".card");
+    card.classList.add('fadeIn');
+});
---
+++ chamber/scripts/timestamp.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let now = new Date().toISOString();
+
+    
+    document.getElementById("timestamp").value = now;
+});
