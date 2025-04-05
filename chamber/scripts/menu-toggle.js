1150082fa683ee0f749be62c58220251430aa0ef
Norlando_jr <norlandojr@hotmail.com>
2025-04-04 22:49:46 -0300

    fix the menu in mobile apps 2

--- chamber/scripts/menu-toggle.js
+++ chamber/scripts/menu-toggle.js
@@ -1,5 +1,14 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
+
+
 menuToggle.addEventListener('click', function() {
     menu.classList.toggle('active');
+});
+
+
+document.addEventListener('click', function(event) {
+    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+        menu.classList.remove('active');
+    }
 });
